f53b27c5d74a319ad02697be80a7664aed545b35
Sabbir Hosen <167640727+sabbir-hosen-dev@users.noreply.github.com>
2024-05-19 01:41:53 +0600

    Update index.js

--- index.js
+++ index.js
@@ -28,8 +28,87 @@ const client = new MongoClient(uri, {
 
 async function run() {
 
-  try{
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
 
+    const collection = client
+      .db(process.env.DB_NAME)
+      .collection(process.env.DB_COLLESTER);
+
+    const userCollection = client
+      .db(process.env.DB_NAME)
+      .collection("select-volunteer");
+
+    app.post("/postEvent", async (req, res) => {
+      try {
+        const data = await collection.insertOne(req.body);
+        res.send(data);
+      } catch (err) {
+        console.log(err);
+        res.status(500).send("Error adding event");
+      }
+    });
+
+    app.delete("/deleteAttendEvent/:id", async (req, res) => {
+      try {
+        const data = await userCollection.deleteOne({ _id: new ObjectId(req.params.id) });
+        res.send(data);
+      } catch (error) {
+        console.log(error);
+        res.status(500).send("Error deleting event");
+      }
+    });
+
+    app.get("/attendEvent", async (req, res) => {
+      try {
+        const data = await userCollection.find({}).toArray();
+        res.send(data);
+      } catch (error) {
+        console.log(error);
+        res.status(500).send("Error fetching events");
+      }
+    });
+
+    app.delete("/deleteItem/:id", async (req, res) => {
+      try {
+        const data = await userCollection.deleteOne({ _id: new ObjectId(req.params.id) });
+        res.send(data);
+      } catch (error) {
+        console.log(error);
+        res.status(500).send("Error deleting item");
+      }
+    });
+
+    app.get("/userData", async (req, res) => {
+      try {
+        const data = await userCollection.find({ email: req.query.email }).toArray();
+        res.send(data);
+      } catch (error) {
+        console.log(error);
+        res.status(500).send("Error fetching user data");
+      }
+    });
+
+    app.post("/addData", async (req, res) => {
+      try {
+        const data = await userCollection.insertOne(req.body);
+        res.send(data);
+      } catch (err) {
+        console.log(err);
+        res.status(500).send("Error adding data");
+      }
+    });
+
+    app.get("/allData", async (req, res) => {
+      try {
+        const data = await collection.find({}).toArray();
+        res.send(data);
+      } catch (error) {
+        console.log(error);
+        res.status(500).send("Error fetching data");
+      }
+    });
     app.get("/", (req, res) => {
       res.send("Hello, I am server");
     });
